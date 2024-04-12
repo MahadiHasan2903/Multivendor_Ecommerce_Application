066bbec41a9db8d490422c7d660fa19e30fee46d
MD. Mahadi Hasan <12mahadihasan@gmail.com>
2024-04-12 19:42:59 +0600

    updated

--- server/controller/Payment.js
+++ server/controller/Payment.js
@@ -1,12 +1,12 @@
 const express = require("express");
-const router = express.Router();
-const catchAsyncErrors = require("../middleware/CatchAsyncErrors");
+const paymentRouter = express.Router();
+const CatchAsyncErrors = require("../middleware/CatchAsyncErrors");
 
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
-router.post(
-  "/process",
-  catchAsyncErrors(async (req, res, next) => {
+paymentRouter.post(
+  "/payment-process",
+  CatchAsyncErrors(async (req, res, next) => {
     const myPayment = await stripe.paymentIntents.create({
       amount: req.body.amount,
       currency: "inr",
@@ -21,11 +21,11 @@ router.post(
   })
 );
 
-router.get(
-  "/stripeapikey",
-  catchAsyncErrors(async (req, res, next) => {
+paymentRouter.get(
+  "/payment-stripeapikey",
+  CatchAsyncErrors(async (req, res, next) => {
     res.status(200).json({ stripeApikey: process.env.STRIPE_API_KEY });
   })
 );
 
-module.exports = router;
+module.exports = paymentRouter;
